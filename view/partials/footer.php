ed2bfd2b2d6f6626609dff181e2c78f26785a57d
BossDols <185933672+zel-asp@users.noreply.github.com>
2026-02-03 01:02:45 +0800

    Refactor user input sanitization and update branding to Iron Pulse Fitness

--- view/partials/footer.php
+++ view/partials/footer.php
@@ -4,8 +4,8 @@
         <div class="grid grid-cols-1 custom_screen:grid-cols-2 md:justify-items-center md:grid-cols-4 gap-8">
             <div>
                 <h3 class="text-gray font-extrabold text-xl md:text-2xl break-words max-w-xs md:max-w-none  mb-4">
-                    PADOGSKEI
-                    <span class="text-brand">WILDGYM</span>
+                    IRONPULSE
+                    <span class="text-brand">FITNESS</span>
                 </h3>
                 <p class="text-gray-400">Gym slogan.</p>
             </div>
@@ -22,22 +22,24 @@
                 <ul class="space-y-2 text-gray-400">
                     <li><?= htmlspecialchars($info['address']) ?></li>
                     <li><?= htmlspecialchars($info['phone']) ?></li>
-                    <li>P<?= htmlspecialchars($info['email']) ?></li>
+                    <li><?= htmlspecialchars($info['email']) ?></li>
                     <li><?= htmlspecialchars($info['name']) ?></li>
                 </ul>
             </div>
             <div>
                 <h3 class="text-gray text-xl font-bold mb-4">Follow Us</h3>
                 <div class="flex space-x-4">
-                    <a href="#" class="text-gray-400 hover:text-brand"><i class="fab fa-tiktok text-xl"></i></a>
-                    <a href="https://www.facebook.com/groups/135169316896319" class="text-gray-400 hover:text-brand"><i
+                    <a href="https://www.tiktok.com/@tchello_ai/video/7589858486369013013?is_from_webapp=1&sender_device=pc"
+                        class="text-gray-400 hover:text-brand"><i class="fab fa-tiktok text-xl"></i></a>
+                    <a href="https://www.facebook.com/janzeldols" class="text-gray-400 hover:text-brand"><i
                             class="fab fa-facebook text-xl"></i></a>
-                    <a href="#" class="text-gray-400 hover:text-brand"><i class="fab fa-instagram text-xl"></i></a>
+                    <a href="https://www.tiktok.com/@itz.reiiii1/video/7596196019851873557?is_from_webapp=1&sender_device=pc"
+                        class="text-gray-400 hover:text-brand"><i class="fab fa-instagram text-xl"></i></a>
                 </div>
             </div>
         </div>
         <div class="border-t border-gray mt-8 pt-8 text-center text-gray-400">
-            <p>&copy; 2025 PadogskieWildgym. All rights reserved.</p>
+            <p>&copy; 2025 JanzelDolo. All rights reserved.</p>
         </div>
     </div>
 </footer>
